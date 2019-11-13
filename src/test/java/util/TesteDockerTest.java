b956a8a8871d8344db2146ab56ef3f426ad281f1
bruno <brunoz.abreu@gmail.com>
2019-11-13 20:31:29 -0300

    aaa

--- src/test/java/util/TesteDockerTest.java
+++ src/test/java/util/TesteDockerTest.java
@@ -5,6 +5,7 @@
  */
 package util;
 
+import junit.framework.Test;
 import junit.framework.TestCase;
 
 /**
@@ -12,36 +13,38 @@ import junit.framework.TestCase;
  * @author brunoz
  */
 public class TesteDockerTest extends TestCase {
-   
+
     private TesteDocker t;
-    
+
     public TesteDockerTest(String testName) {
         super(testName);
     }
 
-    public TesteDockerTest() {
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
         t = new TesteDocker();
     }
-    
-    
 
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
 
     public void testSomarDoisNumeros() {
-       int num1  = 5;
-       int num2  = 10;
-       
-       t.somarDoisNumeros(num1, num2);
-        assertEquals(t.somarDoisNumeros(num1, num2), 15);
-       
+        int num1 = 5;
+        int num2 = 10;
+
+        int soma = t.somarDoisNumeros(num1, num2);
+        assertEquals(soma, (num1 + num2));
     }
 
     public void testMultiplicarDoisNumeros() {
-          
-       int num1  = 5;
-       int num2  = 10;
-       
-       t.somarDoisNumeros(num1, num2);
-        assertEquals(t.somarDoisNumeros(num1, num2), 50);
+        int num1 = 5;
+        int num2 = 10;
+        
+        int mult = t.multiplicarDoisNumeros(num1, num2);
+        assertEquals(mult, (num1 * num2));
     }
-    
+
 }
